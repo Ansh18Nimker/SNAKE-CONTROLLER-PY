9596c45ae8c7ee097952f6a1bf96997e18fcb1d2
Ansh18Nimker <anshnimker2004@gmail.com>
2025-11-02 03:43:01 +0530

    operations on a simple image

--- opencv tutorial/tutorial.py
+++ opencv tutorial/tutorial.py
@@ -1,1 +1,8 @@
 import cv2
+img = cv2.imread('assets/logo.jpeg', 1)
+img = cv2.resize(img, (0,0),fx=2, fy=2)
+img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+cv2.imwrite('new_img.jpeg', img)
+cv2.imshow('Image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
